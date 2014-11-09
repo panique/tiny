d875e362cfbee4630fc30dc6592dbb38b6c7e4f6
Panique <panique@web.de>
2014-11-09 01:01:06 +0100

    renamed folder, added model

--- application/core/controller.php
+++ application/core/controller.php
@@ -47,7 +47,7 @@ class Controller
      */
     public function loadModel()
     {
-        require APP . '/models/model.php';
+        require APP . '/model/model.php';
         // create new "model" (and pass the database connection)
         $this->model = new Model($this->db);
     }
